f7e398a056d15de9d6e70cbf41ee3bfefcc062dd
root <root@shared.jbs.co.il>
2011-07-05 23:09:46 +0300

    module complete

---
+++ test.js
@@ -0,0 +1,24 @@
+var diff_match_patch=require('./index');
+var dmp =new diff_match_patch();
+function launch() {
+  var text1 = "this is some test. blah blah blah";
+  var text2 = "this is other text. blah blah blah";
+  dmp.Diff_Timeout = 1;
+
+
+  var ms_start = (new Date()).getTime();
+  var d = dmp.diff_main(text1, text2);
+  var ms_end = (new Date()).getTime();
+
+  if (true) {
+    dmp.diff_cleanupSemantic(d);
+  }
+  if (false) {
+    dmp.Diff_EditCost = 4;
+    dmp.diff_cleanupEfficiency(d);
+  }
+  //var ds = dmp.diff_prettyHtml(d);
+  //console.log(ds + '<br/>Time: ' + (ms_end - ms_start) / 1000 + 's');
+  console.log(d)
+}
+launch()
