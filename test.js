65b3bbd65825807151630fcf4d2a7e5286496c44
Ryan Graham <r.m.graham@gmail.com>
2012-12-21 20:42:29 -0800

    Add a dump of the exports to the tests, to verify the exports are clean

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var assert = require('assert');
 var diff_match_patch=require('./index');
-var dmp =new diff_match_patch();
+var dmp = new diff_match_patch();
+
+console.dir(diff_match_patch);
+
 function launch() {
   var text1 = "this is some test. blah blah blah";
   var text2 = "this is other text. blah blah blah";
